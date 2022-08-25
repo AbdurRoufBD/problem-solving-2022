4af2d2b485610125e8b7fe4b70f79ddfc11077e9
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-08-25 23:03:56 +0600

    https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/

--- src/leetcode/linkedList/cycledetect/Solution.java
+++ src/leetcode/linkedList/cycledetect/Solution.java
@@ -23,4 +23,32 @@ public class Solution {
     	
     	return false;
     }
+    
+    public ListNode detectCycle(ListNode head) {
+    	//https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+    	int maxValue = 10000 * 10000;
+    	if(head == null) {
+    		return null;
+    	}
+        
+    	
+    	ListNode pointer = head;
+    	ListNode finalNode = null;
+    	while(pointer != null) {
+    		int value = pointer.val;
+    		if(value > 100000) {
+    			pointer.val = pointer.val - maxValue;
+    			finalNode = pointer;
+    			break;
+    		}
+    		
+    		pointer = pointer.next;
+    	}
+    	
+    	if(pointer == null) {
+    		return null;
+    	} else {
+    		return finalNode;
+    	}
+    }
 }
