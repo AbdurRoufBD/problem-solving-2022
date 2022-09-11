f89dd7893a5d4e142fc45b70c85e20f96f0c4431
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-09-12 00:18:57 +0600

    test commit

---
+++ src/leetcode/diagonal_traverse/Solution.java
@@ -0,0 +1,8 @@
+package leetcode.diagonal_traverse;
+
+public class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int[] arr = new int[mat.length * mat[0].length];
+        return arr;
+    }
+}
