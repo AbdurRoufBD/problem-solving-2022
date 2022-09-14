b513276f6e1e1b93f63f4ea16ba23fe21ac53736
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-09-14 19:35:09 +0600

    https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1168/

---
+++ src/leetcode/spiral_matrix/Runner.java
@@ -0,0 +1,17 @@
+package leetcode.spiral_matrix;
+
+
+
+public class Runner {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {11,12,13,14,15},
+                {21,22,23,24,25},
+                {31,32,33,34,35}
+        };
+
+
+
+        new Solution().spiralOrder(mat);
+    }
+}
