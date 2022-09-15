61bf2b4665a557c4ebfa911858b778b0ca612a5e
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-09-16 00:20:09 +0600

    https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/

---
+++ src/leetcode/longest_common_prefix/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.longest_common_prefix;
+
+public class Runner {
+    public static void main(String[] args) {
+        String[] strs = {""};
+        new Solution().longestCommonPrefix(strs);
+    }
+}
---
+++ src/leetcode/longest_common_prefix/Solution.java
@@ -0,0 +1,38 @@
+package leetcode.longest_common_prefix;
+
+public class Solution {
+    int getSmallestLength(String[] strs) {
+        int min = Integer.MAX_VALUE;
+        for(String str : strs) {
+            if(min > str.length()) {
+                min = str.length();
+            }
+        }
+        return min;
+    }
+    public String longestCommonPrefix(String[] strs) {
+        //https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/
+        StringBuilder longCommonPrefixStr = new StringBuilder("");
+        int minLength = getSmallestLength(strs);
+        int i = 0;
+        while (i < minLength) {
+            char firstChar = strs[0].charAt(i);
+            boolean isMatchedWithRest = true;
+            for(int k = 1; k < strs.length; k++) {
+                char c = strs[k].charAt(i);
+                if(firstChar != c) {
+                    isMatchedWithRest = false;
+                    break;
+                }
+            }
+            if(isMatchedWithRest) {
+                longCommonPrefixStr.append(firstChar);
+            } else {
+                break;
+            }
+            i++;
+        }
+        String output = longCommonPrefixStr.toString();
+        return output;
+    }
+}
