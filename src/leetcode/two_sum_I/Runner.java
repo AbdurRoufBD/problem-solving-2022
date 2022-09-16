60cdaf87938067d19d2481615c46766644d3fce2
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-09-16 15:42:45 +0600

    https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1153/

---
+++ src/leetcode/two_sum_I/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.two_sum_I;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,4,9,56,90};
+        new Solution().twoSum(numbers, 8);
+    }
+}
