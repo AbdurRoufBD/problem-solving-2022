ff81bb99775aa463d39a76282abc47d9625ac723
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-09-16 21:18:24 +0600

    https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1164/

---
+++ src/leetcode/reverse_words/Runner.java
@@ -0,0 +1,10 @@
+package leetcode.reverse_words;
+
+
+
+public class Runner {
+    public static void main(String[] args) {
+        String str = "a good   example";
+        new Solution().reverseWords(str);
+    }
+}
