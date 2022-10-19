4ee844973d50d41fe7e36fde16e23764eebad7e7
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-10-20 00:28:40 +0600

    https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1379/

---
+++ src/leetcode/queue_stack/decode_string/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.queue_stack.decode_string;
+
+public class Runner {
+    public static void main(String[] args) {
+        String test = "5[a2[b3[c]]]";
+        new Solution().decodeString(test);
+    }
+}
--- src/leetcode/queue_stack/decode_string/Solution.java
+++ src/leetcode/queue_stack/decode_string/Solution.java
@@ -3,9 +3,47 @@ package leetcode.queue_stack.decode_string;
 import java.util.Stack;
 
 public class Solution {
+    //https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1379/
     public String decodeString(String s) {
-        char[] strArr = s.toCharArray();
-        Stack<Character> stack = new Stack<>();
-        return null;
+        char[] charArray = s.toCharArray();
+        Stack<String> stack = new Stack<>();
+        for(char c : charArray) {
+            if(c == ']') {
+                String seqBuilder = "";
+                while(!stack.isEmpty()) {
+                    if("[".equals(stack.peek())) {
+                        stack.pop();
+                      break;
+                    } else {
+                        seqBuilder = stack.pop() + seqBuilder;
+                    }
+                }
+
+                String numberStr = "";
+                while(!stack.isEmpty()) {
+                    char digit = stack.peek().toCharArray()[0];
+                    if(digit >= '0' && digit <= '9') {
+                        numberStr = stack.pop() + numberStr;
+                    } else {
+                        break;
+                    }
+                }
+
+                int times = Integer.parseInt(numberStr);
+                String finalString = "";
+                for(int i = 0; i < times; i++) {
+                    finalString += seqBuilder;
+                }
+
+                stack.push(finalString);
+            } else {
+                stack.push("" + c);
+            }
+        }
+        String ans = "";
+        while (!stack.isEmpty()) {
+            ans = stack.pop() + ans;
+        }
+        return ans;
     }
 }
