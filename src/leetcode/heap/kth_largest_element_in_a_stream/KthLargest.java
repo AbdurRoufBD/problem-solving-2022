edf150a2e57026566760b9d82cf4f0521eaa04f1
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-10-24 23:57:11 +0600

    https://leetcode.com/explore/learn/card/heap/646/practices/4016/

--- src/leetcode/heap/kth_largest_element_in_a_stream/KthLargest.java
+++ src/leetcode/heap/kth_largest_element_in_a_stream/KthLargest.java
@@ -1,30 +1,29 @@
 package leetcode.heap.kth_largest_element_in_a_stream;
 
-import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Stack;
-
+//https://leetcode.com/explore/learn/card/heap/646/practices/4016/
 public class KthLargest {
-    private PriorityQueue<Integer> maxHeap;
+    private PriorityQueue<Integer> minHeap;
     private int k;
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
         for(int n : nums) {
-            maxHeap.add(n);
+            minHeap.add(n);
         }
+        if(nums.length - k > 0) {
+            for(int i = 0; i < nums.length - k; i++) {
+                minHeap.poll();
+            }
+        }
+
     }
 
     public int add(int val) {
-        maxHeap.add(val);
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < k - 1; i++) {
-            stack.push(maxHeap.poll());
+        minHeap.add(val);
+        if(minHeap.size() == k + 1) {
+            minHeap.poll();
         }
-        int kthElement = maxHeap.peek();
-        while(!stack.isEmpty()) {
-            maxHeap.add(stack.pop());
-        }
-        return kthElement;
+        return minHeap.peek();
     }
 }
