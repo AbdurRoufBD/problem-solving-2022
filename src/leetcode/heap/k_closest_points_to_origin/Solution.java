5c174e7a2526ae317456fed3ef2cd1bd8e5e045d
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-10-26 23:44:39 +0600

    https://leetcode.com/explore/learn/card/heap/646/practices/4088/ (improvement)

--- src/leetcode/heap/k_closest_points_to_origin/Solution.java
+++ src/leetcode/heap/k_closest_points_to_origin/Solution.java
@@ -1,5 +1,7 @@
 package leetcode.heap.k_closest_points_to_origin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 //https://leetcode.com/explore/learn/card/heap/646/practices/4088/
 class DistPointPair implements Comparable<DistPointPair> {
@@ -24,16 +26,16 @@ public class Solution {
 
 
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<DistPointPair> minHeap = new PriorityQueue<>();
+        DistPointPair[] arr = new DistPointPair[points.length];
+        int l = 0;
         for(int[] point : points) {
-            minHeap.add(new DistPointPair(point));
+            arr[l++] = new DistPointPair(point);
         }
-
+        PriorityQueue<DistPointPair> minHeap = new PriorityQueue<>(Arrays.asList(arr));
         int[][] ans = new int[k][2];
         for(int i = 0; i < k; i++) {
             ans[i] = minHeap.poll().point;
         }
-
         return ans;
     }
 }
