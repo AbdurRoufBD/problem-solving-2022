01cfbb0c48b1b76ba8eb3edc1ea813bae42bce26
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-11-03 23:40:32 +0600

    https://leetcode.com/explore/learn/card/binary-search/125/template-i/952/

--- src/leetcode/binary_search/search_in_rotated_sorted_array/Runner.java
+++ src/leetcode/binary_search/search_in_rotated_sorted_array/Runner.java
@@ -2,7 +2,7 @@ package leetcode.binary_search.search_in_rotated_sorted_array;
 
 public class Runner {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,8,9, 1};
-        new Solution().search(arr, 10);
+        int[] arr = {1, 3};
+        System.out.println(new Solution().search(arr, 3));
     }
 }
--- src/leetcode/binary_search/search_in_rotated_sorted_array/Solution.java
+++ src/leetcode/binary_search/search_in_rotated_sorted_array/Solution.java
@@ -1,5 +1,5 @@
 package leetcode.binary_search.search_in_rotated_sorted_array;
-
+//https://leetcode.com/explore/learn/card/binary-search/125/template-i/952/
 public class Solution {
     private int findPivot(int[] nums) {
         int left = 0;
@@ -19,7 +19,27 @@ public class Solution {
     }
     public int search(int[] nums, int target) {
         int pivot = findPivot(nums);
+        int left;
+        int right;
 
-        return pivot;
+        if(nums[0] <= target) {
+            left = 0;
+            right = (nums.length +  pivot - 1) % nums.length;
+        } else {
+            left = pivot;
+            right = nums.length - 1;
+        }
+
+        do {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } while (left <= right);
+        return -1;
     }
 }
