5075c9008694fa53a0899ee67ba0e5df7fd533fe
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-11-05 12:11:32 +0600

    https://leetcode.com/explore/learn/card/binary-search/126/template-ii/948/

---
+++ src/leetcode/binary_search/find_peak_element/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.binary_search.find_peak_element;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,9};
+        System.out.println(new Solution().findPeakElement(nums));
+    }
+}
