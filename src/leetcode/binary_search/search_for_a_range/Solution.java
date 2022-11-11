4be664d3a46c89c47cf1e024eb79ffd00ab94adb
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-11-11 23:42:19 +0600

    https://leetcode.com/explore/learn/card/binary-search/135/template-iii/944/

--- src/leetcode/binary_search/search_for_a_range/Solution.java
+++ src/leetcode/binary_search/search_for_a_range/Solution.java
@@ -3,52 +3,36 @@ package leetcode.binary_search.search_for_a_range;
 import java.util.Objects;
 
 public class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 1 && nums[0] == target) {
-            return new int[]{0, 0};
-        }
-        int startIdx = -1;
-        int endIdx = -1;
-
+    //https://leetcode.com/explore/learn/card/binary-search/135/template-iii/944/
+    int findBound(int[] nums, int target, boolean isFirst) {
         int left = 0;
         int right = nums.length - 1;
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(target == nums[mid]) {
-                int midLeft = mid - 1;
-                if(midLeft >= 0 && target != nums[midLeft] || midLeft < 0) {
-                    startIdx = mid;
-                    break;
+        while(left <= right) {
+            int mid = left + (right - left) / 2 ;
+            if(nums[mid] == target) {
+                if(isFirst) {
+                    if(left == mid || nums[mid - 1] != target) {
+                        return mid;
+                    } else {
+                        right = mid - 1;
+                    }
                 } else {
-                    right = mid;
+                    if(right == mid || nums[mid + 1] != target) {
+                        return mid;
+                    } else {
+                        left = mid + 1;
+                    }
                 }
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-
-
-        left = 0;
-        right = nums.length - 1;
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(target == nums[mid]) {
-                int midRight = mid + 1;
-                if(midRight < nums.length && target != nums[midRight] || midRight >= nums.length) {
-                    endIdx = mid;
-                    break;
-                } else {
-                    left = mid + 1;
-                }
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return new int[]{startIdx, endIdx};
+        return -1;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[] {findBound(nums, target, true), findBound(nums, target, false)};
+        return ans;
     }
 }
