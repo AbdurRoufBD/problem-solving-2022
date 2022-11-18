68f820cff55ded97dd831192df04e3242456b790
Abdur Rouf <38908221+AbdurRoufBD@users.noreply.github.com>
2022-11-18 10:31:05 +0000

    collection libray is perfectly working in codespaces

--- src/leetcode/test_code_spaces/test.java
+++ src/leetcode/test_code_spaces/test.java
@@ -1,6 +1,18 @@
 package leetcode.test_code_spaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello from dhaka");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.forEach(e -> {
+            System.out.println(e);
+        });
+
+        System.out.println("Hehe done");
+
     }
 }
