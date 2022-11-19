c415d79d9e79ed428bfffbc145bebf1d46254d25
Abdur Rouf <38908221+AbdurRoufBD@users.noreply.github.com>
2022-11-19 09:43:45 +0000

    https://leetcode.com/explore/learn/card/binary-search/137/conclusion/978/

---
+++ src/leetcode/binary_search/perfect_square/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.binary_search.perfect_square;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPerfectSquare(14));
+        //System.out.println(Math.sqrt(Integer.MAX_VALUE));
+    }
+}
