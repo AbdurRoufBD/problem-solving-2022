0dd6ee5a926a62b59f21fafe527e365ba533356e
AbdurRouf <1505097.ar@ugrad.cse.buet.ac.bd>
2022-12-08 20:34:41 +0600

    https://leetcode.com/explore/learn/card/binary-search/146/more-practices-ii/1040/

--- src/leetcode/binary_search/median_of_two_sorted_arrays/Runner.java
+++ src/leetcode/binary_search/median_of_two_sorted_arrays/Runner.java
@@ -2,8 +2,8 @@ package leetcode.binary_search.median_of_two_sorted_arrays;
 
 public class Runner {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 6, 7, 8};
-		int[] arr2 = {11};
+		int[] arr = {1, 2};
+		int[] arr2 = {3, 4};
 		System.out.println(new Solution().findMedianSortedArrays(arr, arr2));;
 	}
 }
--- src/leetcode/binary_search/median_of_two_sorted_arrays/Solution.java
+++ src/leetcode/binary_search/median_of_two_sorted_arrays/Solution.java
@@ -3,44 +3,33 @@ package leetcode.binary_search.median_of_two_sorted_arrays;
 public class Solution {
 	//https://leetcode.com/explore/learn/card/binary-search/146/more-practices-ii/1040/
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int left = 0;
-		int right = nums1.length;
-		int total = nums1.length + nums2.length;
-		boolean isEven;
-		int median;
-		if(total % 2 == 0) {
-			median = total / 2;
-			isEven = true;
-		} else {
-			median = (total + 1) / 2;
-			isEven = false;
+		int len1 = nums1.length;
+		int len2 = nums2.length;
+		if(len2 < len1) {
+			return findMedianSortedArrays(nums2, nums1);
 		}
-		while(left <= right) {
-			int cut1 = left + (right - left) / 2;
-			if(cut1 > median) {
-				cut1 = median;
-			} else if(cut1 < 0) {
-				cut1 = 0;
-			} else {
-				cut1 = cut1;
-			}
-			int cut2 = median - cut1;
-			int cut1Left = (cut1 >= 1 && cut1 <= nums1.length) ? nums1[cut1 - 1] : Integer.MIN_VALUE;
-			int cut1Right = (cut1 >= 0 && cut1 < nums1.length) ? nums1[cut1] : Integer.MAX_VALUE;
-			int cut2Left = (cut2 >= 1 && cut2 <= nums2.length) ? nums2[cut2 - 1] : Integer.MIN_VALUE;
-			int cut2Right = (cut2 >= 0 && cut2 < nums2.length) ? nums2[cut2] : Integer.MAX_VALUE;
-			if(cut1Left > cut2Right) {
-				right = cut1 - 1;
-			} else if(cut2Left > cut1Right) {
-				left = cut1 + 1;
-			} else {
-				if(isEven) {
-					return (Math.max(cut1Left, cut2Left) + Math.min(cut1Right, cut2Right)) / 2.0;
+		int low = 0;
+		int high = len1;
+		while(low <= high) {
+			int cut1 = low + (high - low)/2;
+			int cut2 = (len1 + len2 + 1) / 2 - cut1;
+			int l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+			int l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+			int r1 = cut1 == len1 ? Integer.MAX_VALUE : nums1[cut1];
+			int r2 = cut2 == len2 ? Integer.MAX_VALUE : nums2[cut2];
+			if(l1 <= r2 && l2 <= r1) {
+				if((len1 + len2) % 2 == 0) {
+					return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
 				} else {
-					return Math.max(cut1Left, cut2Left);
+					return Math.max(l1, l2);
 				}
+			} else if(l1 > r2) {
+				high = cut1 - 1;
+			} else if(l2 > r1) {
+				low = cut1 + 1;
 			}
 		}
+		
 		return 0;
     }
 }
