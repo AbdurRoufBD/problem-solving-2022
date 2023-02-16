6520cae47914d84283e5c60ddda92bc6f3bb605f
abdur rouf <abdurrouf@abdurs-MBP.lan>
2023-02-16 01:06:59 -0800

    test commit

--- src/leetcode/array_and_string/add_binary/Runner.java
+++ src/leetcode/array_and_string/add_binary/Runner.java
@@ -2,9 +2,10 @@ package leetcode.array_and_string.add_binary;
 
 public class Runner {
     public static void main(String[] args) {
-        String a = "101010101";
+        /*String a = "101010101";
         String b = "11111111111";
-        new Solution().addBinary(a, b);
+        new Solution().addBinary(a, b);*/
+        System.out.println("Hello world");
     }
 }
 
