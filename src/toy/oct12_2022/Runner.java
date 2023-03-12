f823074edb3e9209a18857ae372ee265bcfd7def
rouf <1505097.ar@ugrad.buet.ac.bd>
2023-03-11 23:16:34 -0800

    test commit

--- src/toy/oct12_2022/Runner.java
+++ src/toy/oct12_2022/Runner.java
@@ -2,19 +2,21 @@ package toy.oct12_2022;
 
 public class Runner {
     public static void main(String[] args) {
-        Calculation calculationObject = new Calculation();
+//        Calculation calculationObject = new Calculation();
+//
+//
+//        MyCalculation myCalculationObject = new MyCalculation(); // good practice
+//        // sub class variable                  subclass object
+//
+//
+//        Calculation calculation = new MyCalculation(); // bad practice
+//        // super class variable    subclass object
+//
+//
+//        //property or field
+//        //method
 
-
-        MyCalculation myCalculationObject = new MyCalculation(); // good practice
-        // sub class variable                  subclass object
-
-
-        Calculation calculation = new MyCalculation(); // bad practice
-        // super class variable    subclass object
-
-
-        //property or field
-        //method
+        System.out.println("Hello world");
 
     }
 }
